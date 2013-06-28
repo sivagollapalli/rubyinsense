d3d485aae887cdcd6384e05d5b55ac63f046604b
rishi jain <rishi@joshsoftware.com>
2013-06-28 17:56:03 +0530

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubyinsense::Application.config.secret_token = '94557cbc409c59a572fd8b96c25de37cc9f03871441ef6391f2eb83957580d402d8edbe5e6992a9321102762c7d7fbf3dbb306e03ded226a094bb7f37c209efa'
