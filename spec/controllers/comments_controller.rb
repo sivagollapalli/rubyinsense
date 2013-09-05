57ed6b3919f48f07dfab162ea6ec6607dece14d0
rishi jain <rishi@joshsoftware.com>
2013-09-05 18:48:11 +0530

    added factories, model specs

---
+++ spec/controllers/comments_controller.rb
@@ -0,0 +1,13 @@
+require 'specs_helper'
+
+describe CommentsController do
+  context "if user is not logged in, he cant" do
+    it "create a comment"
+    it "destroy a comment"
+  end
+
+  context "if user is logged in, he can" do
+    it "create a comment"
+    it "destroy a comment"
+  end
+end
